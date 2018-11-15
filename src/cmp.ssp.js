8fffccc61fb16d799f5ce1292fc8d29e762f31cc
Andrew <andrew@digitalenterprises.nl>
2018-11-15 09:20:01 +0100

    add semiconlon

--- src/cmp.ssp.js
+++ src/cmp.ssp.js
@@ -212,7 +212,7 @@ function weborama() {
 			"https://opt.objectiveportal.com/pixel.gif?customer=LOT&brand=staatsloterij&domain=NL&process=banner&utm_source=stl-wv-ndm-fixedscroll&utm_medium=bac&utm_campaign=stl-werving",
 			"https://ad.doubleclick.net/ddm/trackimp/N479001.2276702NEXTDAYMEDIANL/B10757124.215703095;dc_trk_aid=414721216;dc_trk_cid=98572431;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=?"
 		]};
-		document.write('<scr'+'ipt type="text/javascript" src="https://media.adrcdn.com/scripts/w-display/screenad_launch_1.0.0_scrambled.js"></scr'+'ipt>');`
+		document.write('<scr'+'ipt type="text/javascript" src="https://media.adrcdn.com/scripts/w-display/screenad_launch_1.0.0_scrambled.js"></scr'+'ipt>');`;
 
 	let div = document.createElement("div");
 	div.style.display = "inline-block";
