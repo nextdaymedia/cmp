def11c6332c48cf5e81545deea4f1d582b0c6b5e
Michael Cabot <michael@digitalenterprises.nl>
2019-07-15 16:33:18 +0200

    Use en translations as default
    Do not overwrite with empty strings

--- src/lib/localize.js
+++ src/lib/localize.js
@@ -19,10 +19,17 @@ export class Localize {
 		const localizedMap = this.processLocalized(localizedData);
 		const currentLocal = findLocale();
 		const [language] = currentLocal.split('-');
-		this.localizedValues = {
-			...localizedMap[language],
-			...localizedMap[currentLocal]
-		};
+		this.localizedValues = {...localizedMap['en']};
+		for (const [key, value] of Object.entries(localizedMap[language])) {
+			if (value !== '') {
+				this.localizedValues[key] = value;
+			}
+		}
+		for (const [key, value] of Object.entries(localizedMap[currentLocal])) {
+			if (value !== '') {
+				this.localizedValues[key] = value;
+			}
+		}
 	}
 
 	lookup = key => {
--- src/lib/translations.js
+++ src/lib/translations.js
@@ -2,7 +2,7 @@
  * The default set of translated pieces of text indexed by locale.
  * Values from window.__cmp.config.localization will override these
  * per locale.  Empty values will use the english value provided
- * inline in each component. Because the purpose definitions will change,
+ * below under the 'en' key. Because the purpose definitions will change,
  * you will need to update the translations regularly. As a consequence, this
  * translations.js file is very important to keep up to date.
  */
