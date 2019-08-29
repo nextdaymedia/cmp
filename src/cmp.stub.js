052e67ae6b0cab1a1188fa3c31ca20db5fb75373
Michael Cabot <michael@digitalenterprises.nl>
2019-08-29 16:19:47 +0200

    Request non personalized google ads

--- src/cmp.stub.js
+++ src/cmp.stub.js
@@ -55,3 +55,11 @@ window.ndmtag = window.ndmtag || {};
 window.ndmtag.cmd = window.ndmtag.cmd || [];
 
 window.addEventListener("message", listener, false);
+
+// fetch google personalization consent information
+window.__cmp('getGooglePersonalization', (consent, isSuccess) => {
+	// request non personalized ads if we don't have a cookie or if no consent is given
+	if (!isSuccess || !consent.googlePersonalizationData.consentValue) {
+		(window.adsbygoogle=window.adsbygoogle||[]).requestNonPersonalizedAds=1;
+	}
+});
