4cbd936b43487263ea5781edb3a01de01ec54108
Michael Cabot <michael@digitalenterprises.nl>
2020-05-06 14:47:45 +0200

    Remove unused code

--- src/lib/cmp.js
+++ src/lib/cmp.js
@@ -2,7 +2,7 @@
 // Remove support for TCF v1 once it is no longer supported by the community.
 //
 // Functions in this file should register the callback with both __cmp and __tcfapi.
-// The loaded CMP will only implement one of the two, ensuring the callback is only called once.
+// The loaded CMP must only implement one of the two functions, ensuring the callback is only called once.
 import log from './log';
 
 export const getTCData = (view, callback) => {
@@ -21,19 +21,3 @@ export const getTCData = (view, callback) => {
 		});
 	}
 };
-
-// export const getGooglePersonalization = (view, callback) => {
-// 	if (view.__tcfapi === undefined && view.__cmp === undefined) {
-// 		return log.error('__tcfapi and __cmp are both undefined');
-// 	}
-//
-// 	if (view.__tcfapi !== undefined) {
-// 		view.__tcfapi('getTCData', 2, (data, success) => {
-//
-// 		})
-// 	}
-// 	if (view.__cmp !== undefined) {
-// 		log.warn('__cmp is deprecated: use __tcfapi instead');
-// 		view.__cmp('getGooglePersonalization', callback);
-// 	}
-// }
