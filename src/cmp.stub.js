f19baebfebcc0b6efdddad0e9b63aae43bb19f00
Michael Cabot <michael@digitalenterprises.nl>
2020-05-07 11:29:24 +0200

    Print warning inside callback

--- src/cmp.stub.js
+++ src/cmp.stub.js
@@ -1,6 +1,7 @@
 // The quantcast script (scr/template/quantcast.js) is prepended to the bundle of this file (cmp.stub.bundle.js).
 
 import listener from "./lib/ssp-fallback";
+import log from "./lib/log";
 
 window.ndmtag = window.ndmtag || {};
 window.ndmtag.cmd = window.ndmtag.cmd || [];
@@ -10,6 +11,7 @@ window.addEventListener("message", listener, false);
 if (window.__cmp !== undefined) {
 	// fetch google personalization consent information
 	window.__cmp('getGooglePersonalization', (consent, isSuccess) => {
+		log.warn('__cmp is deprecated: use __tcfapi instead');
 		// request non personalized ads if we don't have a cookie or if no consent is given
 		if (!isSuccess || !consent.googlePersonalizationData.consentValue) {
 			(window.adsbygoogle=window.adsbygoogle||[]).requestNonPersonalizedAds=1;
--- src/lib/cmp.js
+++ src/lib/cmp.js
@@ -1,7 +1,7 @@
 // This file supports both TCF v1 (__cmp) and TCF v2 (__tcfapi).
 // Remove support for TCF v1 once it is no longer supported by the community.
 //
-// Functions in this file should register the callback with both __cmp and __tcfapi.
+// Functions in this file should register the callback with both __cmp and __tcfapi, if possible.
 // The loaded CMP must only implement one of the two functions, ensuring the callback is only called once.
 import log from './log';
 
@@ -14,8 +14,8 @@ export const getTCData = (view, callback) => {
 		view.__tcfapi('getTCData', 2, callback);
 	}
 	if (view.__cmp !== undefined) {
-		log.warn('__cmp is deprecated: use __tcfapi instead');
 		view.__cmp('getConsentData', null, data => {
+			log.warn('__cmp is deprecated: use __tcfapi instead');
 			const success = data.gdprApplies && data.consentData;
 			callback({gdprApplies: data.gdprApplies, tcString: data.consentData}, success);
 		});
